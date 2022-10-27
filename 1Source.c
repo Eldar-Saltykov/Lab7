b74433f4d87dd07b07f990a043053a5397aacb54
Eldar-Saltykov <112901922+Eldar-Saltykov@users.noreply.github.com>
2022-10-27 10:58:55 +0300

    Update 1Source.c

--- 1Source.c
+++ 1Source.c
@@ -5,19 +5,40 @@ int main()
 {
 	char c;
 	setlocale(LC_ALL, "RUS");
-	printf("Введите цифру '1' or букву 'a' :");
+	printf("Введите цифру или букву  :");
 	scanf("%c",&c); 
 	switch (c)
 	{
 		case '1' :
 			printf("Это цифра.\n");
 			break;
-		case 'a' :
-			printf("Это буква.\n");
+		case '2' :
+			printf("Это цифра.\n");
+			break; 
+                case '3' :
+			printf("Это цифра.\n");
 			break;
-		default:
+                case '4' :
+			printf("Это цифра.\n");
+			break;
+                case '5' :
+			printf("Это цифра.\n");
+			break;
+                case '6' :
+			printf("Это цифра.\n");
+			break;
+                case '7' :
+			printf("Это цифра.\n");
+			break;
+                case '8' :
+			printf("Это цифра.\n");
+			break;
+                case '9' :
+			printf("Это цифра.\n");
+			break;
 
-			printf("Неизвестный символ\n");	
+		default:
+			printf("Это буква. \n");	
 	}
 		
 	return 0;
